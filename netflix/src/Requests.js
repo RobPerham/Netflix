a65fa7f3e3f235308989f6124e7a0f42d9857c68
Rob Perham <robperham15@gmail.com>
2024-10-14 21:03:55 +0100

    Add Banner and Row components; integrate Axios for API requests

---
+++ netflix/src/Requests.js
@@ -0,0 +1,15 @@
+// typiically you would store this in a {process.env.API_KEY} but for this project we will hard code it
+const API_KEY = "ecd094e3ef3b28faa2739217add8d5cd";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
